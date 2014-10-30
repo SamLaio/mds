156f30aa6d7a53d1f99e3dc063af5a05cbbf664e
SamLaio <f85122@gmail.com>
2014-10-30 17:12:46 +0800

    Signed-off-by: SamLaio <f85122@gmail.com>

--- control/plan.php
+++ control/plan.php
@@ -8,4 +8,21 @@ class plan {
 	public function GetPlan(){
 		echo json_encode($this->db->GetPlan());
 	}
+	public function PlanAdd($arr=false){
+		if(isset($arr['post'])){
+			$arr = $arr['post'];
+			/*
+				PlanAddSeq
+				PlanAddDate
+				PlanAddEarnings
+			*/
+			foreach($arr as $key => $value){
+				$key = explode('PlanAdd',$key);
+				$key = strtolower($key[1]);
+				$tmp[$key] = $value;
+			}
+			// print_r($tmp);exit;
+			$this->db->PlanAdd($tmp);
+		}
+	}
 }
--- model/plan.php
+++ model/plan.php
@@ -10,66 +10,37 @@ class ModelPlan  extends LibDataBase {
 		$flow = $this->Assoc($this->table,'*',"user_id = '".$_SESSION['UserId']."'",'in_date asc');
 		$plan = $this->Assoc($this->PlanTable,'*',"user_id = '".$_SESSION['UserId']."'",'in_date asc');
 		$count = 0;
-		if(count($plan) >= 1){
-			foreach($flow as $fK => $fV){
-				$add = '';
-				$de = '';
-				if($fV['type'] == '+'){
-					$add = $fV['amount'];
-					$count += $add;
-				}else{
-					$de = $fV['amount'];
-					$count -= $de;
-				}
-				$re[] = array('date'=>$fV['in_date'],'add'=>(int) $add,'de'=>(int) $de,'count'=>(int) $count,'earnings'=>'');
+		foreach($flow as $fK => $fV){
+			$add = '-';
+			$de = '-';
+			if($fV['type'] == '+'){
+				$add = $fV['amount'];
+				$count += $add;
+			}else{
+				$de = $fV['amount'];
+				$count -= $de;
 			}
-			$tmp = '';
-			foreach($plan as $pK=>$pV){
-				foreach($re as $rK => $rV){
-					if($rV['date'] < $pV['in_date']){
-						$tmp .= ($tmp !='')?",".$rK:$rK;
-					}else{
-						foreach(explode(',',$tmp) as $tV){
-							$re[$tV]['earnings'] = $pV['earnings'];
-						}
-						$tmp = '';
-					}
-				}
-			}
-			/*foreach($plan as $pK=>$pV){
-				$re[] = array('date'=>$pV['in_date'],'add'=>123123123123,'de'=>123123123123,'count'=>'','earnings'=>(int) $pV['earnings']);
-			}
-			array_multisort($re);
-			$tmp = '';
-			$count = 0;
+			$re[] = array('seq'=>$fV['seq'],'date'=>$fV['in_date'],'add'=>(int) $add,'de'=>(int) $de,'count'=>(int) $count,'earnings'=>'');
+		}
+		$tmp = '';
+		foreach($plan as $pK=>$pV){
 			foreach($re as $rK => $rV){
-				if($rV['add'] == $rV['de'] and $rV['add'] == 123123123123){
-					foreach(explode(',',$tmp) as $tV)
-						$re[$tV]['earnings'] = $rV['earnings'];
-					$re[$rK]['add'] = '';
-					$re[$rK]['de'] = '';
-					$re[$rK]['count'] = $count;
-					$tmp = '';
-				}else{
-					$tmp .= ($tmp !='')?",".$rK:$rK;
-					$count = $rV['count'];
+				if($rV['date'] <= $pV['in_date'] and $re[$rK]['earnings'] == ''){
+					$re[$rK]['earnings'] = $pV['earnings'];
 				}
-			}*/
-		}else{
-			foreach($flow as $fK => $fV){
-				$add = '';
-				$de = '';
-				if($fV['type'] == '+'){
-					$add = $fV['amount'];
-					$count += $add;
-				}else{
-					$de = $fV['amount'];
-					$count -= $de;
-				}
-				$re[] = array('date'=>$fV['in_date'],'add'=>$add,'de'=>$de,'earnings'=>'','count'=>$count);
 			}
 		}
-		// print_r($re);
 		return $re;
+	}
+	public function PlanAdd($arr){
+		if($arr['seq'] != ''){
+			$seq = $arr['seq'];
+			unset($arr['seq']);
+			$sql = $this->Up($this->PlanTable,$arr,"seq = '$seq'");
+		}else{
+			unset($arr['seq']);
+			$sql = $this->In($this->PlanTable, $arr);
+		}
+		echo $sql;
 	}
 }
